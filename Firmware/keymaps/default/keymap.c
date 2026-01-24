fa1a150f2d873b48adfa89df3fd3cb6f25f58ac5
super9chriss <schuilchristiaan@gmail.com>
2026-01-24 15:10:29 +0100

    update

--- Firmware/keymaps/default/keymap.c
+++ Firmware/keymaps/default/keymap.c
@@ -3,12 +3,17 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_numpad_4x4(
-        KC_7, KC_8, KC_9, KC_PPLS,  // Rij 0
-        KC_4, KC_5, KC_6, KC_PMNS,  // Rij 1
-        KC_1, KC_2, KC_3, KC_PENT,  // Rij 2
-        KC_0, KC_A, KC_DOT, KC_BSPC // Rij 3 (voorbeeld)
+        KC_P7, KC_P4, KC_P1, KC_P0,  // Rij 0
+        KC_P8, KC_P5, KC_P2, KC_PPLS,  // Rij 1
+        KC_P9, KC_P6, KC_P3, KC_PMNS,  // Rij 2
+        KC_DEL, KC_BSPC, KC_DOT, KC_PENT // Rij 3 (voorbeeld)
     )
 };
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }, // Laag 0: Links = Zachter, Rechts = Harder
+};
+#endif
     /*
      * ┌───┬───┬───┬───┐
      * │ 7 │ 8 │ 9 │   │
@@ -20,3 +25,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │   0   │ . │   │
      * └───────┴───┴───┘
      */
+
+
